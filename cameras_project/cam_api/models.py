9eb6d2fd8a766fff0c53dba4e585caa3631a6217
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2021-08-31 11:33:18 +0300

    Add models and migrate

--- cameras_project/cam_api/models.py
+++ cameras_project/cam_api/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.room_name
+
+
+class Camera(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)  # ??
+
+
+class Logs(models.Model):
+    timestamp = models.IntegerField()
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)  # ??
+    n_people = models.IntegerField('number or people', default=0)
+
+
+class DayRoomStats(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    week = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    max_people = models.IntegerField()
+    max_time = models.DateTimeField()
+    min_people = models.IntegerField()
+    min_time = models.DateTimeField()
+    avg_people = models.IntegerField()
+
+
+class WeekRoomStats(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    week = models.IntegerField()
+    month = models.IntegerField()
+    max_people = models.IntegerField()
+    max_time = models.DateTimeField()
+    min_people = models.IntegerField()
+    min_time = models.DateTimeField()
+    avg_people = models.IntegerField()
+
+
+class MonthRoomStats(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    max_people = models.IntegerField()
+    max_time = models.DateTimeField()
+    min_people = models.IntegerField()
+    min_time = models.DateTimeField()
+    avg_people = models.IntegerField()	
+
+
+class YearRoomStats(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    max_people = models.IntegerField()
+    max_time = models.DateTimeField()
+    min_people = models.IntegerField()
+    min_time = models.DateTimeField()
+    avg_people = models.IntegerField()
--- cameras_project/cameras_project/settings.py
+++ cameras_project/cameras_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .config import Config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-407$f3**+!l0!t15geb7^mqwp&)4$!-@dd%jd3$654p3l$jgp('
+SECRET_KEY = Config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'cameras_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': Config.DB_NAME,
+        'USER': Config.DB_USER,
+        'PASSWORD': Config.DB_PASSWORD,
+        'HOST': Config.DB_HOST,
+        'PORT': Config.DB_PORT
     }
 }
 
